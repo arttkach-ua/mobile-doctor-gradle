d5ba0a33179edcdd6e0fbe43a641ae0369634cfa
arttkach-ua <62747869+arttkach-ua@users.noreply.github.com>
2023-01-01 15:29:50 +0200

    init

---
+++ src/main/java/com/tkach/md/MobileDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.tkach.md;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkach/md/MobileDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkach.md;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
